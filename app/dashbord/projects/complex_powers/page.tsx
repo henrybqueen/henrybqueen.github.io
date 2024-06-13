ff3679e43ce80539da773c44f5eea3e9573f5386
unknown <henrybqueen@gmail.com>
2024-06-13 11:03:45 -0700

    delete dashbord dir, add some config

--- app/dashbord/projects/complex_powers/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Plane from './plane'
-
-export default function Page() {
-    return (
-        <Plane />
-    );
-}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
+    images: {
+      unoptimized: true,
+    },
+    basePath: '/henrybqueen.github.io',
+    assetPrefix: '/henrybqueen.github.io/',
+  };
+  
 
 export default nextConfig;
+
+
+
+  
